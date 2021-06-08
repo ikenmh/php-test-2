8e55bb09dbb2122ca50640c085900f6906f36564
ikenmh <66178390+ikenmh@users.noreply.github.com>
2021-06-08 08:42:49 -0700

    inicio de  repo

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+      hola mundo DEV
+    </div>
+      
+      
+    <div>
+        <ul>
+             <li>op1</li>
+            <li>op1</li>
+            <li>op1</li>
+        </ul>
+    </div>
+</body>
+</html>
